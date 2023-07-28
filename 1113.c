ba857c6e920a8d03e7679e43a8abce42eba9d0ae
Zabir Abdullah <91465257+zabirabdullah@users.noreply.github.com>
2023-07-28 23:43:14 +0600

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    while(1)
+    {
+        scanf("%d%d", &x, &y);
+        if(x>y)
+        {
+            printf("Decrescente\n");
+        }
+        else if(x<y)
+        {
+            printf("Crescente\n");
+        }
+        else if(x==y)
+        {
+            break;
+        }
+    }
+    
+    return 0;
+}
